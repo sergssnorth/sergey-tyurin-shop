ffdb8594a04e416d811b8c09a1b47b3355602ba2
sergss <sergssnorth@mail.ru>
2024-03-09 08:48:55 +0300

    123

---
+++ telegram_bot/bot.py
@@ -0,0 +1,17 @@
+from aiogram import Bot, Dispatcher, executor, types
+
+API_TOKEN = 'Ваш Telegram Bot API токен'
+
+bot = Bot(token=API_TOKEN)
+dp = Dispatcher(bot)
+
+@dp.message_handler(commands=['start', 'help'])
+async def send_welcome(message: types.Message):
+    await message.reply("Привет! Я твой бот.")
+
+@dp.message_handler()
+async def echo(message: types.Message):
+    await message.answer(message.text)
+
+if __name__ == '__main__':
+    executor.start_polling(dp, skip_updates=True)
